6375c9b7f1120f2e4606cd32f781ed63e4639e7d
Evgenii Efremov <johne32rus23@gmail.com>
2019-08-28 21:41:19 +0300

    fix method find_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def find_tests_by_level(level)
-    Test.joins("INNER JOIN user_tests ON user_tests.tests_id = tests.id
-      AND user_tests.users_id = #{id} AND tests.level = #{level}")
+    Test.joins('INNER JOIN user_tests ON user_tests.tests_id = tests.id').where(
+      'user_tests.users_id= :id AND tests.level= :level', id: id, level: level
+    )
   end
 end
