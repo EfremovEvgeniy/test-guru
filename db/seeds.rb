d941086365b9a07ea0b04b201039c86befd63d5b
Evgenii Efremov <johne32rus23@gmail.com>
2019-08-28 21:14:30 +0300

    fix id in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
-Category.create([{ title: 'Frontend' }, { title: 'Backend' }, { title: 'Db' }])
+categories = Category.create!([{ title: 'Frontend' }, { title: 'Backend' }, { title: 'Db' }])
 
-Test.create([{ title: 'Ruby', level: 2, categories_id: 2 },
-             { title: 'React', level: 1, categories_id: 1 },
-             { title: 'HTML/CSS', level: 1, categories_id: 1 },
-             { title: 'SQL', level: 2, categories_id: 3 }])
+tests = Test.create!([{ title: 'Ruby', level: 2, categories_id: categories.second.id },
+                      { title: 'React', level: 1, categories_id: categories.first.id },
+                      { title: 'HTML/CSS', level: 1, categories_id: categories.first.id },
+                      { title: 'SQL', level: 2, categories_id: categories.third.id }])
 
-Question.create [{ body: 'Hashes and arrays', tests_id: 1 },
-                 { body: 'What is style component?', tests_id: 2 },
-                 { body: 'Difference between HTML4 and HTML5', tests_id: 3 },
-                 { body: 'Write example of JOIN query', tests_id: 4 },
-                 { body: 'Difference between lambda and prock', tests_id: 1 }]
+questions = Question.create! [{ body: 'Hashes and arrays', tests_id: tests.first.id },
+                              { body: 'What is style component?', tests_id: tests.second.id },
+                              { body: 'Difference between HTML4 and HTML5', tests_id: tests.third.id },
+                              { body: 'Write example of JOIN query', tests_id: tests.fourth.id },
+                              { body: 'Difference between lambda and prock', tests_id: tests.first.id }]
 
-User.create([{ name: 'Evgenii', email: 'efremoven94@gmail.com' },
-             { name: 'Nikolay', email: 'johne32rus23@gmail.com' },
-             { name: 'Kristina', email: 'example@admin.com' }])
+users = User.create!([{ name: 'Evgenii', email: 'efremoven94@gmail.com' },
+                      { name: 'Nikolay', email: 'johne32rus23@gmail.com' },
+                      { name: 'Kristina', email: 'example@admin.com' }])
 
-Answer.create [{ body: 'Some text about hashes and arrays', correct: true, questions_id: 1 },
-               { body: 'I don know ', questions_id: 2 },
-               { body: 'Some text about HTML', correct: true, questions_id: 3 },
-               { body: 'Wrong JOIN query', questions_id: 4 },
-               { body: 'Some text about prock and lambda', questions_id: 5 }]
+Answer.create! [{ body: 'Some text about hashes and arrays', correct: true, questions_id: questions.first.id },
+                { body: 'I don know ', questions_id: questions.second.id },
+                { body: 'Some text about HTML', correct: true, questions_id: questions.third.id },
+                { body: 'Wrong JOIN query', questions_id: questions.fourth.id },
+                { body: 'Some text about prock and lambda', questions_id: questions.fifth.id }]
 
-UserTest.create [{ users_id: 1, tests_id: 1 },
-                 { users_id: 2, tests_id: 1 },
-                 { users_id: 3, tests_id: 1, passed: true },
-                 { users_id: 3, tests_id: 2, passed: true },
-                 { users_id: 3, tests_id: 3 }]
+UserTest.create! [{ users_id: users.first.id, tests_id: tests.first.id },
+                  { users_id: users.second.id, tests_id: tests.first.id },
+                  { users_id: users.third.id, tests_id: tests.first.id, passed: true },
+                  { users_id: users.third.id, tests_id: tests.second.id, passed: true },
+                  { users_id: users.third.id, tests_id: tests.third.id }]
