986d2bf2913f5e1a7265cde86126fa5d07420704
Evgenii Efremov <johne32rus23@gmail.com>
2019-08-28 07:57:44 +0300

    add method all_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.all_by_category(category)
+    categoty_id = Category.where('title = ?', category.capitalize!).pluck(:id)[0]
+    where('categories_id = ?', categoty_id).order(id: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,23 @@
 
 Category.create([{ title: 'Frontend' }, { title: 'Backend' }, { title: 'DB' }])
 
-Test.create([{ tite: 'Ruby', level: 2, categories_id: 2 },
-             { tite: 'React', level: 1, categories_id: 1 },
-             { tite: 'HTML/CSS', level: 1, categories_id: 1 },
-             { tite: 'SQL', level: 2, categories_id: 3 }])
+Test.create([{ title: 'Ruby', level: 2, categories_id: 2 },
+             { title: 'React', level: 1, categories_id: 1 },
+             { title: 'HTML/CSS', level: 1, categories_id: 1 },
+             { title: 'SQL', level: 2, categories_id: 3 }])
 
-Question.create [{ body: 'Hashes and arrays', test_id: 1 },
-                 { body: 'What is style component?', test_id: 2 },
-                 { body: 'Difference between HTML4 and HTML5', test_id: 3 },
-                 { body: 'Write example of JOIN query', test_id: 4 },
-                 { body: 'Difference between lambda and prock', test_id: 1 }]
+Question.create [{ body: 'Hashes and arrays', tests_id: 1 },
+                 { body: 'What is style component?', tests_id: 2 },
+                 { body: 'Difference between HTML4 and HTML5', tests_id: 3 },
+                 { body: 'Write example of JOIN query', tests_id: 4 },
+                 { body: 'Difference between lambda and prock', tests_id: 1 }]
 
 User.create([{ name: 'Evgenii', email: 'efremoven94@gmail.com' },
              { name: 'Nikolay', email: 'johne32rus23@gmail.com' },
              { name: 'Kristina', email: 'example@admin.com' }])
 
-Answer.create [{ body: 'Some text about hashes and arrays', correct: true, question_id: 1 },
-               { body: 'I don know ', question_id: 2 },
-               { body: 'Some text about HTML', correct: true, question_id: 3 },
-               { body: 'Wrong JOIN query', question_id: 4 },
-               { body: 'Some text about prock and lambda', question_id: 5 }]
+Answer.create [{ body: 'Some text about hashes and arrays', correct: true, questions_id: 1 },
+               { body: 'I don know ', questions_id: 2 },
+               { body: 'Some text about HTML', correct: true, questions_id: 3 },
+               { body: 'Wrong JOIN query', questions_id: 4 },
+               { body: 'Some text about prock and lambda', questions_id: 5 }]
