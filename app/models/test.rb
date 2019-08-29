e04cabd5ef14c2e7d05fb1f2270d344051c8c7a2
Evgenii Efremov <johne32rus23@gmail.com>
2019-08-29 19:49:21 +0300

    fix query construct

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
   def self.all_by_category(category)
-    joins('INNER JOIN categories ON tests.categories_id = categories.id').where(
-      categories: { title: category }
-    ).order(id: :desc).pluck(:title)
+    joins('INNER JOIN categories ON tests.categories_id = categories.id')
+      .where(categories: { title: category })
+      .order(id: :desc)
+      .pluck(:title)
   end
 end
