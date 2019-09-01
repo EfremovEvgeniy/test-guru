960a2e5fb0004dd5e2cdf58dddb516bdf50ca56e
Evgenii Efremov <johne32rus23@gmail.com>
2019-09-01 19:39:37 +0300

    fix dependences

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_many :questions, dependent: :destroy
-  has_many :tests_users, dependent: :nullify
+  has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
   def self.all_by_category(category_name)
