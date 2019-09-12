cef03713608c4ecec7bad35ca53da25f450bbe98
Evgenii Efremov <johne32rus23@gmail.com>
2019-09-12 08:09:44 +0300

    require first and last name on sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(_tests)
-    admin_tests_path(current_user)
+    if current_user.is_a?(Admin)
+      admin_tests_path(current_user)
+    else
+      root_path
+    end
   end
 end
