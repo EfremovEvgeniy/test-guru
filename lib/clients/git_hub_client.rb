522ab6a640cb6cfecf4bb799a7f1ac8096d0a0c0
Evgenii Efremov <johne32rus23@gmail.com>
2019-09-17 20:33:36 +0300

    add client and service

---
+++ lib/clients/git_hub_client.rb
@@ -0,0 +1,21 @@
+class GitHubClient < Octokit::Client
+
+  ACCESS_TOKEN = '45d3353aa724631140275eb4c8b63913c2cac97a'.freeze
+
+  def initialize
+    @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post('gists', params) do |request|
+      request.headers['Content-Type'] = 'application/json'
+      reauest.body = params.to_json
+    end
+  end
+
+  private
+
+  def setup_http_client
+    Octokit::Client.new(:access_token => ACCESS_TOKEN)
+  end
+end
