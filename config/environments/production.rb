806cbcfed625217174b5b2080774eb5fcc3ee6d0
Evgenii Efremov <johne32rus23@gmail.com>
2019-09-26 06:24:22 +0300

    add config for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
+
+
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
@@ -60,7 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_option = {}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587, 
+  #   user_name: ENV['SMTP_USERNAME'],
+  #   password: ENV['SMTP_PASSWORD'],
+  #   authentication: 'plain', 
+  #   enable_starttls_auto: true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
