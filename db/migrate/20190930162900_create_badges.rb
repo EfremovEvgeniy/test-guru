6fa6f8a7a57b39f5b612ef36840ad98613b719a3
Evgenii Efremov <johne32rus23@gmail.com>
2019-09-30 19:31:14 +0300

    add badge model

---
+++ db/migrate/20190930162900_create_badges.rb
@@ -0,0 +1,12 @@
+class CreateBadges < ActiveRecord::Migration[6.0]
+  def change
+    create_table :badges do |t|
+      t.string :title, null: false
+      t.string :image_url, null: false
+      t.string :rule, null: false
+      t.string :param, null: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_918_032_254) do
+ActiveRecord::Schema.define(version: 20_190_930_162_900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20_190_918_032_254) do
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'question_id'
     t.index ['question_id'], name: 'index_answers_on_question_id'
+  end
+
+  create_table 'badges', force: :cascade do |t|
+    t.string 'title', null: false
+    t.string 'image_url', null: false
+    t.string 'rule', null: false
+    t.string 'param'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
   create_table 'categories', force: :cascade do |t|
